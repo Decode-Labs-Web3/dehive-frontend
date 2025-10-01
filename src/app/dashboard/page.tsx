594fdbc869bb5c2c45588c05275e32ef6707af52
vutranquangminh <19112004vutranquangminh@gmail.com>
2025-10-01 13:58:08 +0700

    setup success

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,8 @@
-export default function Home() {
+export default function Dashboard() {
   return (
     <main className="min-h-dvh grid place-items-center p-6">
       <div className="rounded-2xl border p-8 shadow">
-        <h1 className="text-3xl font-bold mb-2">Tailwind OK ✅</h1>
+        <h1 className="text-3xl font-bold mb-2">This is dashboard</h1>
       </div>
     </main>
   );
